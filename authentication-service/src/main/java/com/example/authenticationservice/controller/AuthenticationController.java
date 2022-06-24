534f1c5b582c4d0391aa1ca5f02be621aa2f770c
TodoranIonut <todoran.ionut96@gmail.com>
2022-06-24 10:29:34 +0300

    Authentication service update

--- authentication-service/src/main/java/com/example/authenticationservice/controller/AuthenticationController.java
+++ authentication-service/src/main/java/com/example/authenticationservice/controller/AuthenticationController.java
@@ -1,4 +1,29 @@
 package com.example.authenticationservice.controller;
 
+import com.example.authenticationservice.domain.AppUser;
+import com.example.authenticationservice.request.AppUserRegisterRequestBody;
+import com.example.authenticationservice.service.AuthenticationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
 public class AuthenticationController {
+
+    private final AuthenticationService authenticationService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AppUser> saveUser(@RequestBody AppUserRegisterRequestBody newUser){
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/auth/register").toUriString());
+        return ResponseEntity.created(uri).body(authenticationService.saveAppUser(newUser));
+    }
+
 }
--- authentication-service/src/main/java/com/example/authenticationservice/security/SecurityConfig.java
+++ authentication-service/src/main/java/com/example/authenticationservice/security/SecurityConfig.java
@@ -1,13 +1,44 @@
 package com.example.authenticationservice.security;
 
 
+import com.example.authenticationservice.filter.AppUserAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
+    private final UserDetailsService userDetailsService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
+        auth.userDetailsService(userDetailsService);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.authorizeRequests().anyRequest().permitAll();
+        http.addFilter(new AppUserAuthenticationFilter(authenticationManagerBean()));
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception{
+        return super.authenticationManagerBean();
+    }
 }
